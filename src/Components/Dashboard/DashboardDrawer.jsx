5b67afa12af9b2f8b3eae8dd6add01348f3b6779
Roberto E. Zubieta <roberto@decent.bet>
2018-01-30 17:01:13 -0500

    Refactoring Drawer into smaller pieces

--- src/Components/Dashboard/DashboardDrawer.jsx
+++ src/Components/Dashboard/DashboardDrawer.jsx
@@ -1,12 +1,7 @@
 import React from 'react'
-import {
-    MuiThemeProvider,
-    Drawer,
-    FlatButton,
-    List,
-    ListItem
-} from 'material-ui'
-import { CopyToClipboard } from 'react-copy-to-clipboard'
+import { MuiThemeProvider, Drawer, List, ListItem } from 'material-ui'
+
+import DashboardDrawerHeader from './DashboardDrawerHeader.jsx'
 import Helper from '../Helper'
 import Themes from './../Base/Themes'
 
@@ -17,6 +12,7 @@ const constants = require('../Constants')
 
 const versionNumber = require('../../../package.json').version
 
+// Simple list item for an URL link
 function CustomListItem({ label, icon, url }) {
     return (
         <ListItem
@@ -29,150 +25,124 @@ function CustomListItem({ label, icon, url }) {
     )
 }
 
-export default function DashboardDrawer({
-    isOpen,
-    onAddressCopiedListener,
-    onChangeContractTypeListener,
-    onLogoutListener,
-    onToggleDrawerListener,
-    onExportPrivateKeyDialogListener,
-    selectedContractType,
-    walletAddress
-}) {
-    return (
-        <MuiThemeProvider muiTheme={themes.getDrawer()}>
-            <Drawer
-                docked={false}
-                width={300}
-                open={isOpen}
-                onRequestChange={open => onToggleDrawerListener(open)}
-            >
-                <div className="container drawer">
+// Dashboard Drawer
+export default class DashboardDrawer extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.renderTokenVersionListItem = this.renderTokenVersionListItem.bind(
+            this
+        )
+    }
+
+    // Builds the items for the version switcher
+    renderTokenVersionListItem(label, version) {
+        let isSelected = version === this.props.selectedContractType
+        let className = `menu-item ${isSelected ? 'selected' : ''}`
+        return (
+            <ListItem
+                key={version}
+                className={className}
+                onClick={() => this.props.onChangeContractTypeListener(version)}
+                primaryText={
                     <div className="row">
-                        <div className="col-12 mt-4 hidden-sm-up">
-                            <FlatButton
-                                label="X"
-                                labelStyle={styles.drawerToggle}
-                                className="float-right"
-                                onClick={() => onToggleDrawerListener(false)}
-                            />
-                        </div>
-                        <div className="col-12">
-                            <img
-                                className="logo"
-                                src={
-                                    process.env.PUBLIC_URL +
-                                    '/assets/img/logos/dbet-white.png'
-                                }
-                                alt="Decent.bet Logo"
-                            />
+                        <div className="col-8">
+                            <p>{label}</p>
                         </div>
                     </div>
-                </div>
-                <div className="row">
-                    <div className="col-12 hidden-sm-up">
-                        <p className="address-mobile">
-                            {walletAddress}
-                            <CopyToClipboard
-                                text={walletAddress}
-                                onCopy={onAddressCopiedListener}
-                            >
-                                <span className="fa fa-clipboard color-gold ml-2 clickable menu-icon" />
-                            </CopyToClipboard>
-                        </p>
-                    </div>
-                </div>
-                <List>
-                    <ListItem
-                        primaryText="Export Private Key"
-                        className="menu-item"
-                        style={styles.menuItem}
-                        leftIcon={<span className={`fa fa-key menu-icon`} />}
-                        onClick={onExportPrivateKeyDialogListener}
+                }
+            />
+        )
+    }
+
+    render() {
+        let {
+            isOpen,
+            onAddressCopiedListener,
+            onLogoutListener,
+            onToggleDrawerListener,
+            onExportPrivateKeyDialogListener,
+            walletAddress
+        } = this.props
+        return (
+            <MuiThemeProvider muiTheme={themes.getDrawer()}>
+                <Drawer
+                    docked={false}
+                    width={300}
+                    open={isOpen}
+                    onRequestChange={open => onToggleDrawerListener(open)}
+                >
+                    <DashboardDrawerHeader
+                        onToggleDrawerListener={onToggleDrawerListener}
+                        onAddressCopiedListener={onAddressCopiedListener}
+                        walletAddress={walletAddress}
                     />
-                    <CustomListItem
-                        label="Buy DBETs"
-                        icon="shopping-cart"
-                        url="https://www.decent.bet/buy"
-                    />
-                    <CustomListItem
-                        label="DBET News"
-                        icon="newspaper-o"
-                        url="https://www.decent.bet/news"
-                    />
-                    <CustomListItem
-                        label="Support"
-                        icon="question"
-                        url="https://www.decent.bet/support"
-                    />
-                    <ListItem
-                        className="menu-item"
-                        primaryText="Token Versions"
-                        style={styles.menuItem}
-                        leftIcon={
-                            <span className={'fa fa-code-fork menu-icon'} />
-                        }
-                        initiallyOpen={false}
-                        primaryTogglesNestedList={true}
-                        nestedItems={[
-                            <ListItem
-                                key={2}
-                                className={`menu-item 
-                                    ${
-                                        constants.TOKEN_TYPE_DBET_TOKEN_NEW ===
-                                        selectedContractType
-                                            ? 'selected'
-                                            : ''
-                                    }`}
-                                primaryText={
-                                    <div className="row">
-                                        <div className="col-8">
-                                            <p>V2 (CURRENT)</p>
-                                        </div>
-                                    </div>
-                                }
-                                onClick={() =>
-                                    onChangeContractTypeListener(
-                                        constants.TOKEN_TYPE_DBET_TOKEN_NEW
-                                    )
-                                }
-                            />,
-                            <ListItem
-                                key={1}
-                                className={`menu-item 
-                                ${
-                                    constants.TOKEN_TYPE_DBET_TOKEN_OLD ===
-                                    selectedContractType
-                                        ? 'selected'
-                                        : ''
-                                }`}
-                                primaryText="V1 (INITIAL)"
-                                onClick={() =>
-                                    onChangeContractTypeListener(
-                                        constants.TOKEN_TYPE_DBET_TOKEN_OLD
-                                    )
-                                }
-                            />
-                        ]}
-                    />
-                    <CustomListItem
-                        label="Token Info"
-                        icon="info"
-                        url="https://www.decent.bet/token/info"
-                    />
-                    <ListItem
-                        primaryText="Log Out"
-                        className="menu-item"
-                        style={styles.menuItem}
-                        leftIcon={<span className={`fa fa-sign-out menu-icon`} />}
-                        onClick={onLogoutListener}
-                    />
-                    <CustomListItem 
-                        label={`Wallet Version: ${versionNumber}`} 
-                        icon='microchip'
+                    <List>
+                        <ListItem
+                            primaryText="Export Private Key"
+                            className="menu-item"
+                            style={styles.menuItem}
+                            leftIcon={
+                                <span className={`fa fa-key menu-icon`} />
+                            }
+                            onClick={onExportPrivateKeyDialogListener}
                         />
-                </List>
-            </Drawer>
-        </MuiThemeProvider>
-    )
+                        <CustomListItem
+                            label="Buy DBETs"
+                            icon="shopping-cart"
+                            url="https://www.decent.bet/buy"
+                        />
+                        <CustomListItem
+                            label="DBET News"
+                            icon="newspaper-o"
+                            url="https://www.decent.bet/news"
+                        />
+                        <CustomListItem
+                            label="Support"
+                            icon="question"
+                            url="https://www.decent.bet/support"
+                        />
+                        <ListItem
+                            className="menu-item"
+                            primaryText="Token Versions"
+                            style={styles.menuItem}
+                            leftIcon={
+                                <span className={'fa fa-code-fork menu-icon'} />
+                            }
+                            initiallyOpen={false}
+                            primaryTogglesNestedList={true}
+                            nestedItems={[
+                                this.renderTokenVersionListItem(
+                                    'V2 (CURRENT)',
+                                    constants.TOKEN_TYPE_DBET_TOKEN_NEW
+                                ),
+                                this.renderTokenVersionListItem(
+                                    'V1 (INITIAL)',
+                                    constants.TOKEN_TYPE_DBET_TOKEN_OLD
+                                )
+                            ]}
+                        />
+                        <CustomListItem
+                            label="Token Info"
+                            icon="info"
+                            url="https://www.decent.bet/token/info"
+                        />
+                        <ListItem
+                            primaryText="Log Out"
+                            className="menu-item"
+                            style={styles.menuItem}
+                            leftIcon={
+                                <span className={`fa fa-sign-out menu-icon`} />
+                            }
+                            onClick={onLogoutListener}
+                        />
+                        <CustomListItem
+                            label={`Wallet Version: ${versionNumber}`}
+                            icon="microchip"
+                        />
+                    </List>
+                </Drawer>
+            </MuiThemeProvider>
+        )
+    }
 }
---
+++ src/Components/Dashboard/DashboardDrawerHeader.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { FlatButton } from 'material-ui'
+import { CopyToClipboard } from 'react-copy-to-clipboard'
+
+const styles = require('../Base/styles').styles
+
+export default function DashboardDrawerHeader({
+    onAddressCopiedListener,
+    onToggleDrawerListener,
+    walletAddress
+}) {
+    return (
+        <div>
+            <div className="container drawer">
+                <div className="row">
+                    <div className="col-12 mt-4 hidden-sm-up">
+                        <FlatButton
+                            label="X"
+                            labelStyle={styles.drawerToggle}
+                            className="float-right"
+                            onClick={() => onToggleDrawerListener(false)}
+                        />
+                    </div>
+                    <div className="col-12">
+                        <img
+                            className="logo"
+                            src={
+                                process.env.PUBLIC_URL +
+                                '/assets/img/logos/dbet-white.png'
+                            }
+                            alt="Decent.bet Logo"
+                        />
+                    </div>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-12 hidden-sm-up">
+                    <p className="address-mobile">
+                        {walletAddress}
+                        <CopyToClipboard
+                            text={walletAddress}
+                            onCopy={onAddressCopiedListener}
+                        >
+                            <span className="fa fa-clipboard color-gold ml-2 clickable menu-icon" />
+                        </CopyToClipboard>
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
