a5764a7714842926d5bd5d68c8e815c279e3584c
Roberto E. Zubieta <roberto@decent.bet>
2018-02-05 10:03:52 -0500

    Extracted the Private Route from App component

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,54 +1,20 @@
 import React, { Component, Fragment } from 'react'
-import { BrowserRouter, Route, Switch, Redirect, withRouter } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import LogoutRoute from './App/LogoutRoute'
+import PrivateRoute from './App/PrivateRoute'
 
 import ConfirmationDialog from './Base/Dialogs/ConfirmationDialog'
 import Helper from './Helper'
-import Web3Loader from './Base/Web3Loader'
 import Login from './Login'
 import NewWallet from './NewWallet/NewWallet'
 import Dashboard from './Dashboard'
-import KeyHandler from './Base/KeyHandler'
 
 const helper = new Helper()
-const keyHandler = new KeyHandler()
-const web3Loader = new Web3Loader()
 
 const DIALOG_UPDATE_AVAILABLE = 0,
     DIALOG_UPDATE_INSTALLED = 1
 
 let ipcRenderer
-
-// Protects a route using a 'Login' system.
-// Inspiration: https://reacttraining.com/react-router/web/example/auth-workflow
-function PrivateRoute({ component: Component, ...rest }) {
-    return (
-        <Route
-            {...rest}
-            render={props => {
-
-                // Do Login
-                if (keyHandler.isLoggedIn()) {
-
-                    // User logged in
-                    web3Loader.init()
-                    return <Component {...props} />
-                } else {
-
-                    // Redirect to login screen
-                    return (
-                        <Redirect
-                            to={{
-                                pathname: '/login',
-                                state: { from: props.location }
-                            }}
-                        />
-                    )
-                }
-            }}
-        />
-    )
-}
 
 class App extends Component {
     constructor(props) {
