8d1c39cad49b3a7358dd29eee0daee4c1a74d12c
Roberto E. Zubieta <roberto@decent.bet>
2018-02-07 13:29:30 -0500

    Flattened App.jsx

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -38,65 +38,59 @@ class App extends Component {
     }
 
     initAutoUpdateListener = () => {
-        const self = this
         ipcRenderer = window.require('electron').ipcRenderer
-        ipcRenderer.on('updateAvailable', function() {
-            self.helpers().toggleDialog(DIALOG_UPDATE_AVAILABLE, true)
-        })
-        ipcRenderer.on('updateReady', function() {
-            self.helpers().toggleDialog(DIALOG_UPDATE_INSTALLED, true)
-        })
+        ipcRenderer.on(
+            'updateAvailable',
+            this.onOpenUpdateAvailableDialogListener
+        )
+        ipcRenderer.on('updateReady', this.onOpenUpdateReadyDialogListener)
     }
 
-    helpers = () => {
-        const self = this
-        return {
-            toggleDialog: (type, open) => {
-                let dialogs = self.state.dialogs
-                if (type == DIALOG_UPDATE_AVAILABLE)
-                    dialogs.update.available.open = open
-                else if (type == DIALOG_UPDATE_INSTALLED)
-                    dialogs.update.installed.open = open
-                self.setState({
-                    dialogs: dialogs
-                })
-            },
-            quitAndInstall: () => {
-                self.helpers().toggleDialog(DIALOG_UPDATE_INSTALLED, false)
-                if (helper.isElectron()) ipcRenderer.send('quitAndInstall')
-            }
+    toggleDialog = (type, open) => {
+        let dialogs = this.state.dialogs
+        if (type === DIALOG_UPDATE_AVAILABLE) {
+            dialogs.update.available.open = open
+        } else if (type === DIALOG_UPDATE_INSTALLED) {
+            dialogs.update.installed.open = open
+        }
+        this.setState({ dialogs: dialogs })
+    }
+
+    onQuitAndInstallListener = () => {
+        this.toggleDialog(DIALOG_UPDATE_INSTALLED, false)
+        if (helper.isElectron()) {
+            ipcRenderer.send('quitAndInstall')
         }
     }
 
-    dialogs = () => {
-        const self = this
-        return {
-            updateAvailable: () => {
-                const closeDialog = () => {
-                    self.helpers().toggleDialog(DIALOG_UPDATE_AVAILABLE, false)
-                }
-                return (
-                    <ConfirmationDialog
-                        title="Update Notification"
-                        message="A new update was detected and will be downloaded in the background."
-                        open={self.state.dialogs.update.available.open}
-                        onClose={closeDialog}
-                        onClick={closeDialog}
-                    />
-                )
-            },
-            updateInstalled: () => {
-                return (
-                    <ConfirmationDialog
-                        title="Update Notification"
-                        message="A new update was detected and has been downloaded. It will now be installed."
-                        open={self.state.dialogs.update.installed.open}
-                        onClose={self.helpers().quitAndInstall}
-                        onClick={self.helpers().quitAndInstall}
-                    />
-                )
-            }
-        }
+    onOpenUpdateAvailableDialogListener = () =>
+        this.toggleDialog(DIALOG_UPDATE_AVAILABLE, true)
+    onOpenUpdateReadyDialogListener = () =>
+        this.toggleDialog(DIALOG_UPDATE_INSTALLED, true)
+    onCloseUpdateAvailableDialogListener = () =>
+        this.toggleDialog(DIALOG_UPDATE_AVAILABLE, false)
+
+    renderUpdateAvailableDialog = () => {
+        return (
+            <ConfirmationDialog
+                title="Update Notification"
+                message="A new update was detected and will be downloaded in the background."
+                open={this.state.dialogs.update.available.open}
+                onClose={this.onCloseUpdateAvailableDialogListener}
+                onClick={this.onCloseUpdateAvailableDialogListener}
+            />
+        )
+    }
+    renderUpdateInstalledDialog = () => {
+        return (
+            <ConfirmationDialog
+                title="Update Notification"
+                message="A new update was detected and has been downloaded. It will now be installed."
+                open={this.state.dialogs.update.installed.open}
+                onClose={this.onQuitAndInstallListener}
+                onClick={this.onQuitAndInstallListener}
+            />
+        )
     }
 
     render() {
@@ -111,8 +105,8 @@ class App extends Component {
                     </Switch>
                 </BrowserRouter>
 
-                {this.dialogs().updateAvailable()}
-                {this.dialogs().updateInstalled()}
+                {this.renderUpdateAvailableDialog()}
+                {this.renderUpdateInstalledDialog()}
             </Fragment>
         )
     }
