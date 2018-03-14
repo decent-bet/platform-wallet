e7b060669482a4d385c94fa3ce29a59b9d19a5b3
Roberto E. Zubieta <roberto@decent.bet>
2018-03-14 09:25:25 -0500

    Added missing test packages

--- src/Components/App/__tests__/App.test.js
+++ src/Components/App/__tests__/App.test.js
@@ -1,7 +1,7 @@
-// import React from 'react'
-// import {shallow} from 'enzyme'
+ import React from 'react'
+ import {shallow} from 'enzyme'
 //
-// import App from '../App'
+ import App from '../App'
 // TODO: fix Cannot find module './build/Release/scrypt' from 'index.js'
 describe.skip('Components/App', function () {
     it('should render without throwing an error', function () {
