743cbb800824e518c396fdf17b7067bc4e13af03
Rogelio Morrell <rogelio@decent.bet>
2018-08-30 10:25:29 -0500

    migrated VET Learn more to material-ui 1

--- src/Components/Wallet/Dialogs/VETLearnMoreDialog.jsx
+++ src/Components/Wallet/Dialogs/VETLearnMoreDialog.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Dialog, Button } from '@material-ui/core'
+import { Dialog, Button, DialogTitle, DialogContent, DialogActions } from '@material-ui/core'
 import { MuiThemeProvider } from 'material-ui'
 import Themes from '../../Base/Themes'
 import { FormattedMessage } from 'react-intl'
@@ -9,52 +9,53 @@ export default function VETLearnMoreDialog({ isOpen, onCloseListener }) {
     return (
         <MuiThemeProvider muiTheme={themes.getDialog()}>
             <Dialog
-                title="DBET Token Upgrade to VET Information"
-                actions={
+                open={isOpen}
+                scroll={"body"}
+                onClose={onCloseListener}
+            >
+                <DialogTitle>DBET Token Upgrade to VET Information</DialogTitle>
+                <DialogContent>
+                    <p>
+                        <FormattedMessage
+                            id="src.Components.Wallet.Dialogs.LearnMoreDialog.Upgraded"
+                            description="Token contract has been upgraded in LearnMoreDialog"
+                        />
+                    </p>
+                    <ul>
+                        <li>
+                            <FormattedMessage
+                                id="src.Components.Wallet.Dialogs.LearnMoreDialog.RemovalOfCrowdsale"
+                                description="Removal of crowdsale in LearnMoreDialog"
+                            />
+                        </li>
+                        <li>
+                            <FormattedMessage
+                                id="src.Components.Wallet.Dialogs.LearnMoreDialog.SwitchFromThrows"
+                                description="Switch from throws in LearnMoreDialog"
+                            />
+                        </li>
+                        <li>
+                            <FormattedMessage
+                                id="src.Components.Wallet.Dialogs.LearnMoreDialog.UsersCanNotTransfer"
+                                description="Users can not transfer in LearnMoreDialog"
+                            />
+                        </li>
+                    </ul>
+                    <p>
+                        <FormattedMessage
+                            id="src.Components.Wallet.Dialogs.LearnMoreDialog.AllTokensWillBeUpgraded"
+                            description="All tokens will be upgraded in LearnMoreDialog"
+                        />
+                    </p>
+                </DialogContent>
+                <DialogActions>
                     <Button
-                        label="Ok"
-                        primary={false}
                         onClick={onCloseListener}
                         onTouchTap={onCloseListener}
-                    />
-                }
-                modal={false}
-                open={isOpen}
-                autoScrollBodyContent={true}
-                onRequestClose={onCloseListener}
-            >
-                <p>
-                    <FormattedMessage
-                        id="src.Components.Wallet.Dialogs.LearnMoreDialog.Upgraded"
-                        description="Token contract has been upgraded in LearnMoreDialog"
-                    />
-                </p>
-                <ul>
-                    <li>
-                        <FormattedMessage
-                            id="src.Components.Wallet.Dialogs.LearnMoreDialog.RemovalOfCrowdsale"
-                            description="Removal of crowdsale in LearnMoreDialog"
-                        />
-                    </li>
-                    <li>
-                        <FormattedMessage
-                            id="src.Components.Wallet.Dialogs.LearnMoreDialog.SwitchFromThrows"
-                            description="Switch from throws in LearnMoreDialog"
-                        />
-                    </li>
-                    <li>
-                        <FormattedMessage
-                            id="src.Components.Wallet.Dialogs.LearnMoreDialog.UsersCanNotTransfer"
-                            description="Users can not transfer in LearnMoreDialog"
-                        />
-                    </li>
-                </ul>
-                <p>
-                    <FormattedMessage
-                        id="src.Components.Wallet.Dialogs.LearnMoreDialog.AllTokensWillBeUpgraded"
-                        description="All tokens will be upgraded in LearnMoreDialog"
-                    />
-                </p>
+                    >
+                        Ok
+                    </Button>
+                </DialogActions>
             </Dialog>
         </MuiThemeProvider>
     )
