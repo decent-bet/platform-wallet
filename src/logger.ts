cab5c4e33379b30ff00e10dcb4da5e2e024b06b9
Rogelio Morrell <rogelio@decent.bet>
2018-09-20 11:38:08 -0500

    missing commit

--- src/logger.ts
+++ src/logger.ts
@@ -1,9 +1,3 @@
-<<<<<<< HEAD:src/logger.ts
-// From: https://cloud.google.com/logging/docs/setup/nodejs#using_winston
-const winston = require('winston')
-const Logger = winston.Logger
-=======
->>>>>>> development:src/logger.js
 const pkg = require('../package')
 const request = require('axios')
 const url = 'https://us-central1-dbet-platform.cloudfunctions.net/electron-error-logger'
@@ -19,15 +13,8 @@ const logger = {
             version: pkg.version,
         }
 
-<<<<<<< HEAD:src/logger.ts
-const logger = new Logger({
-    level: 'error',
-    transports: [new winston.transports.Console(), loggingWinston]
-})
-=======
         request.post(url, data).then()
     }
 }
->>>>>>> development:src/logger.js
 
 module.exports = logger
