a22e5008652185bd272bdec9d5a2ab0e5e645707
Rogelio Morrell <molekilla@gmail.com>
2018-09-25 14:58:04 -0500

    updates to Learn More dialog for vechain tokens

--- src/Components/Wallet/Dialogs/VETLearnMoreDialog.jsx
+++ src/Components/Wallet/Dialogs/VETLearnMoreDialog.jsx
@@ -16,38 +16,28 @@ export default function VETLearnMoreDialog({ isOpen, onCloseListener }) {
             <DialogTitle>DBET Token Upgrade to VET Information</DialogTitle>
             <DialogContent>
             <DialogContentText>
-            <Typography>
+            <Typography align="justify" paragraph={true} variant="body2">
                     <FormattedMessage
-                        id="src.Components.Wallet.Dialogs.LearnMoreDialog.Upgraded"
-                        description="Token contract has been upgraded in LearnMoreDialog"
+                        id="src.Components.Wallet.Dialogs.VETLearnMoreDialog.Upgraded_1"
+                        description="Token contract has been upgraded in LearnMoreDialog_1"
+                    />
+                </Typography>                
+                <Typography align="justify" paragraph={true} variant="body2">
+                    <FormattedMessage
+                        id="src.Components.Wallet.Dialogs.VETLearnMoreDialog.Upgraded_2"
+                        description="Token contract has been upgraded in LearnMoreDialog_1"
                     />
                 </Typography>
-                <Typography>
-                <ul>
-                    <li>
-                        <FormattedMessage
-                            id="src.Components.Wallet.Dialogs.LearnMoreDialog.RemovalOfCrowdsale"
-                            description="Removal of crowdsale in LearnMoreDialog"
-                        />
-                    </li>
-                    <li>
-                        <FormattedMessage
-                            id="src.Components.Wallet.Dialogs.LearnMoreDialog.SwitchFromThrows"
-                            description="Switch from throws in LearnMoreDialog"
-                        />
-                    </li>
-                    <li>
-                        <FormattedMessage
-                            id="src.Components.Wallet.Dialogs.LearnMoreDialog.UsersCanNotTransfer"
-                            description="Users can not transfer in LearnMoreDialog"
-                        />
-                    </li>
-                </ul>
-                </Typography>
-                <Typography>
+                <Typography align="justify" paragraph={true} variant="body2">
                     <FormattedMessage
-                        id="src.Components.Wallet.Dialogs.LearnMoreDialog.AllTokensWillBeUpgraded"
-                        description="All tokens will be upgraded in LearnMoreDialog"
+                        id="src.Components.Wallet.Dialogs.VETLearnMoreDialog.Upgraded_3"
+                        description="Token contract has been upgraded in LearnMoreDialog_1"
+                    />
+                </Typography>
+                <Typography align="justify" paragraph={true} variant="body2">
+                    <FormattedMessage
+                        id="src.Components.Wallet.Dialogs.VETLearnMoreDialog.Upgraded_4"
+                        description="Token contract has been upgraded in LearnMoreDialog_1"
                     />
                 </Typography>
             </DialogContentText>
